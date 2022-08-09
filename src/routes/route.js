678e6bd92da1e57648f099ef61b5956c5f01b48b
Shaily-Tripathi <tripathishaily95@gmail.com>
2022-08-09 20:32:13 +0530

    modified

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,5 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
-
 const router = express.Router();
-
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
-});
-
 router.get("/sol1", function(req, res){
     let arr =[1,2,3,5,6,7]
     function missingNumber(arr) {
@@ -31,7 +18,7 @@ console.log(missingNumber(arr))
 })
 
 router.get("/sol2", function(req, res){
-    let array =[33,34,35,37,38]
+    let array =[7,8,1,2,4,5]
 function missingNum(array) {
     let sum =0
     for(let i=0;i<array.length;i++)
@@ -49,82 +36,4 @@ console.log(missingNum(array))
     res.send('Dummy Response')
 })
 
-
-
-router.get("/shoes", function(req, res){
-    let queryParams = req.query
-    let brand = queryParams.brand
-    res.send("dummy response")
-})
-
-// uses query params
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
-
-// use path param
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
-
-router.get("/films", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-       //send all the films
-      res.send(films) 
-})
-
-router.get("/films/:filmId", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-
-       let filmId = req.params.filmId
-
-       //iterate all the films
-       //search for a film whose id matches with the id recevied in request
-       for(let i = 0; i < films.length; i++){
-           let film = films[i]
-           if(film.id == filmId) {
-               //if there is a match return the response from here
-               return res.send(film)
-           }
-       }
-
-       //if there is no match give an error response
-       res.send("The film id doesn't match any movie")
-})
-
 module.exports = router;
-// adding this comment for no reason
