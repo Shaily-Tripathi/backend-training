7de97bb2240bc5c7c539f1f09cc6cd8ce02d6dfd
Shaily-Tripathi <tripathishaily95@gmail.com>
2022-08-10 12:11:06 +0530

    changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+
 router.get("/sol1", function(req, res){
     let arr =[1,2,3,5,6,7]
     function missingNumber(arr) {
     let sum =0
     for(let i=0;i<arr.length;i++)
         {
-            sum += arr[i]
+            sum += arr[i]// sum = sum+arr[i]
         }
     let n = arr.length+1
     let sumc = Math.floor((n*(n+1))/2);
@@ -17,13 +19,14 @@ console.log(missingNumber(arr))
     res.send('Dummy Response')
 })
 
+
 router.get("/sol2", function(req, res){
-    let array =[7,8,1,2,4,5]
+    let array =[33,34,35,37,38]
 function missingNum(array) {
     let sum =0
     for(let i=0;i<array.length;i++)
         {
-            sum += array[i]
+            sum += array[i] //sum=sum+array[i]
         }
     let n = array.length+1
     let firstNum = array[0]
