c976b2d10acfd5499458d8c14393910737c08747
Shaily-Tripathi <tripathishaily95@gmail.com>
2022-08-26 17:45:12 +0530

    Save Changes

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,82 +1,4 @@
-// const { count } = require("console")
-// const BookModel = require("../models/orderModel")
-
-// const createBook= async function (req, res) {
-//     let data= req.body
-
-//     let savedData= await BookModel.create(data)
-//     res.send({msg: savedData})
-// }
-
-
-
-
-
-
-// const getBooksData = async function (req, res) {
-//     let allBooks = await BookModel.find({ authorName: "HO" })
-//     console.log(allBooks)
-//     if (allBooks.length > 0) res.send({ msg: allBooks, condition: true })
-//     else res.send({ msg: "No books found", condition: false })
-// }
-
-
-// const updateBooks = async function (req, res) {
-//     let data = req.body // {sales: "1200"}
-//     // let allBooks= await BookModel.updateMany( 
-//     //     { author: "SK"} , //condition
-//     //     { $set: data } //update in data
-//     //  )
-//     let allBooks = await BookModel.findOneAndUpdate(
-//         { authorName: "ABC" }, //condition
-//         { $set: data }, //update in data
-//         { new: true, upsert: true } ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT
-//     )
-
-//     res.send({ msg: allBooks })
-// }
-
-// const deleteBooks = async function (req, res) {
-//     // let data = req.body 
-//     let allBooks = await BookModel.updateMany(
-//         { authorName: "FI" }, //condition
-//         { $set: { isDeleted: true } }, //update in data
-//         { new: true } ,
-//     )
-
-//     res.send({ msg: allBooks })
-// }
-
-
-
-// const totalSalesPerAuthor = async function (req, res) {
-//     // let data = req.body 
-//     let allAuthorSales = await BookModel.aggregate(
-//         [
-//             { $group: { _id: "$authorName", totalNumberOfSales: { $sum: "$sales" } } },
-//             { $sort: { totalNumberOfSales: -1 } }
-//         ]
-//     )
-
-//     res.send({ msg: allAuthorSales })
-// }
-
-
-
-
-// // CRUD OPERATIONS:
-// // CREATE
-// // READ
-// // UPDATE
-// // DELETE
-
-
-
-// module.exports.createBook = createBook
-// module.exports.getBooksData = getBooksData
-// module.exports.updateBooks = updateBooks
-// module.exports.deleteBooks = deleteBooks
-// module.exports.totalSalesPerAuthor = totalSalesPerAuthor
+const moment = require('moment');
 const orderModel = require("../models/orderModel")
 const userModel = require("../models/userModel")
 const productModel = require("../models/productModel")
@@ -87,6 +9,7 @@ const createOrder= async function (req, res) {
     let productID = data.productId
     let checkIdOfUser = await userModel.findById(userID)
     let checkIdOfProduct = await productModel.findById(productID)
+    
     if(!userID)
     {
         return res.send({status: false,msg: "User ID must be present"})
@@ -107,18 +30,41 @@ const createOrder= async function (req, res) {
     }
 
     let valHeader = req.headers["isfreeappuser"]
-    if(valHeader==true)
+    if(valHeader==="false"){
+    //User Balance
+    let userB = await userModel.findById(userID)
+    let userBalance = userB["balance"]
+    //Product Price
+    let productP = await productModel.findById(productID)
+    let productPrice = productP.price
+
+    if(userBalance >= productPrice)
     {
-        
+            let usersNewBalance = userBalance - productPrice
+            await userModel.findOneAndUpdate(
+                {_id : userID},
+                {$set : {balance : usersNewBalance}}
+            );
+            let todayDate = moment().format('YYYY-MM-DD')
+            data['amount']=productPrice;
+            data['date']=todayDate;
+            let savedData=await orderModel.create(data);
+            res.send({OrderPlaced : savedData});
     }
     else
     {
-            
+            return res.send({"msg":"Users balance is not enough, cannot make the order"})
     }
-   
-    let savedData= await orderModel.create(data)
-    res.send({data: savedData})
-
+    } 
+    else if(valHeader=="true")
+    {
+        let todayDate = moment().format('YYYY-MM-DD')
+        data['amount']=0;
+        data['date']=todayDate;
+        let savedData=await orderModel.create(data);
+        res.send({OrderPlaced : savedData});
+    
+    }
     
 }
 
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -4,32 +4,17 @@ const headerCheck = function(req, res, next){
     {
         return res.send({msg:" The request is missing a mandatory header"})
     }
-    next()
+    else
+    {
+       if(valHeader=='true'){
+        req.body.isFreeAppUser = true
+       }
+       else if(valHeader ==='false'){
+        req.body.isFreeAppUser = false
+       }
+       next()
+    }
+   
 }
 
 module.exports.headerCheck= headerCheck
-// const mid1= function ( req, res, next) {
-//     req.falana= "hi there. i am adding something new to the req object"
-//     console.log("Hi I am a middleware named Mid1")
-//     next()
-// }
-
-// const mid2= function ( req, res, next) {
-//     console.log("Hi I am a middleware named Mid2")
-//     next()
-// }
-
-// const mid3= function ( req, res, next) {
-//     console.log("Hi I am a middleware named Mid3")
-//     next()
-// }
-
-// const mid4= function ( req, res, next) {
-//     console.log("Hi I am a middleware named Mid4")
-//     next()
-// }
-
-// module.exports.mid1= mid1
-// module.exports.mid2= mid2
-// module.exports.mid3= mid3
-// module.exports.mid4= mid4
